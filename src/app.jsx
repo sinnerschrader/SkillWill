a40f51823514dfb0d66d8a70c76f4c37ec5a1772
katstr <katharina.strecker@sinnerschrader.com>
2016-12-22 12:02:30 +0100

    -show Searchbar - add some test-styles

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import styles from './styles.less';
 import React from 'react';
 import Header from './components/header.jsx';
+import SearchBar from './components/searchBar.jsx';
 import Loader from './components/loader.jsx';
 
 export default class App extends React.Component {
@@ -8,6 +9,7 @@ export default class App extends React.Component {
     return (
       <div>
         <Header />
+        <SearchBar />
       </div>
     )
   }
