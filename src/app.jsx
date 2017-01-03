2de9eadc9bf80b18eba28e779b47f71347fc1a96
katstr <katharina.strecker@sinnerschrader.com>
2017-01-03 21:52:33 +0100

    remove test.jsx loader

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,6 @@ import styles from './styles.less';
 import React from 'react';
 import Header from './components/header.jsx';
 import SearchBar from './components/searchBar.jsx';
-import Loader from './components/loader.jsx';
 
 export default class App extends React.Component {
   render() {
