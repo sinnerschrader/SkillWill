ccf189347edf2e53f0383657901944b6cb549797
Katharina Strecker <katharina.strecker@sinnerschrader.com>
2017-03-28 16:21:51 +0200

    - added userId cookie for nav-link

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
+import Cookies from 'react-cookie';
 
 export default class Header extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isNavOpen: false
+            isNavOpen: false,
+            userId: undefined
         }   
         this.handleClick = this.handleClick.bind(this);
+        this.checkUserIdCookie = this.checkUserIdCookie.bind(this);
+
+        this.checkUserIdCookie();
     }
 
     handleClick(e){
         this.setState({
             isNavOpen: !this.state.isNavOpen
         })
+        this.checkUserIdCookie();
+    }
+
+    checkUserIdCookie() {
+      let u =  Cookies.load("user");
+      if (u != this.state.userId) {
+        this.setState({userId: u})
+      }
+      return !!u;
     }
 
     render() {
@@ -35,7 +49,7 @@ export default class Header extends React.Component {
                                 <a class="nav-link" href="/">Suche</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/my-profile/">Dein Profil</a>
+                                <a class="nav-link" href={`/my-profile/${this.state.userId}`}>Dein Profil</a>
                             </li>
                         </ul>
                     </nav>
