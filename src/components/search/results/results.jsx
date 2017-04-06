c7ffbc0d55d0668975f0b4cf7cc67c7755e8d2d6
Felix G. Lehmann <felix-g.lehmann@sinnerschrader.com>
2017-04-06 12:07:04 +0200

    some code styling changes

--- src/components/search/results/results.jsx
+++ src/components/search/results/results.jsx
@@ -1,44 +1,44 @@
 import React from 'react'
 
 export default class Results extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      resultArray: []
-    }
-    this.scrollToResults = this.scrollToResults.bind(this)
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			resultArray: []
+		}
+		this.scrollToResults = this.scrollToResults.bind(this)
+	}
 
-  scrollToResults(e) {
-    e.preventDefault()
-    const scrollTo = document.getElementsByClassName("results")
-    scrollTo.scrollIntoView()
-  }
+	scrollToResults(e) {
+		e.preventDefault()
+		const scrollTo = document.getElementsByClassName("results")
+		scrollTo.scrollIntoView()
+	}
 
-  render() {
-      if (this.props.results.length != 0) {
-				return(
-					<div class="results-container">
-						<a class="counter" onClick={this.scrollToResults}>
-							<span>{this.props.results.length} Ergebnisse</span>
-						</a>
-						<ul class="results">
-							{this.props.results.map((data, i) => {
-								return (
-									<li class="result-item" key={i}>
-										{React.cloneElement(this.props.children, { data: data })}
-									</li>
-								)
-							})}
-						</ul>
-					</div>
-				)
-      }
-      else {
-      	return (
-          <div class="results-container" data-isEmptyLabel={this.props.noResultsLabel}></div>
-      	)
-    	}
-    }
-  }
+	render() {
+		if (this.props.results.length != 0) {
+			return(
+				<div class="results-container">
+					<a class="counter" onClick={this.scrollToResults}>
+						<span>{this.props.results.length} Ergebnisse</span>
+					</a>
+					<ul class="results">
+						{this.props.results.map((data, i) => {
+							return (
+								<li class="result-item" key={i}>
+									{React.cloneElement(this.props.children, { data: data })}
+								</li>
+							)
+						})}
+					</ul>
+				</div>
+			)
+		}
+		else {
+			return (
+				<div class="results-container" data-isEmptyLabel={this.props.noResultsLabel}></div>
+			)
+		}
+	}
+}
 
