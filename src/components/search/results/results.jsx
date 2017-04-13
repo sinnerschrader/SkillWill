02c22e745cd545f0e17a83e19e6e1fc46e3c3276
Felix G. Lehmann <felix-g.lehmann@sinnerschrader.com>
2017-04-13 15:29:02 +0200

    fix that data object gets mutated

--- src/components/search/results/results.jsx
+++ src/components/search/results/results.jsx
@@ -3,26 +3,74 @@ import React from 'react'
 export default class Results extends React.Component {
 	constructor(props) {
 		super(props)
+		 this.state = {
+      sortCriterion: 'fitness',
+      sortOrder: 'asc'
+    };
 		this.scrollToResults = this.scrollToResults.bind(this)
+		this.sortResults = this.sortResults.bind(this)
+		this.onSortByNameClick = this.onSortByNameClick.bind(this)
+
 	}
+	results = this.props.results
+
+	onSortByNameClick(){
+		this.sortResults('lastName',this.state.sortOrder)
+	}
+
 	scrollToResults(e) {
 		e.preventDefault()
 		const scrollTo = e.target
 		scrollTo.scrollIntoView({behavior: "smooth"})
 	}
 
+	sortResults(criterion, order){
+		this.results = this.props.results.sort((a,b) =>{
+			if(a[criterion] < b[criterion]){
+				return -1
+			} else if(a[criterion] > b[criterion]){
+				return 1
+			} else {
+				if (a.id < b.id){
+					return -1
+				} else {
+					return 1
+				}
+			}
+		})
+		this.forceUpdate()
+	console.log(this.props.results)
+	console.log(this.results)
+	this.setState({
+		sortOrder: this.state.sortOrder
+	})
+	console.log(this.state.sortOrder)
+	}
+
+	shouldComponentUpdate(nextProps, nextState){
+		console.log(nextProps, nextState)
+	}
+	Component = this.props.component
+
 	render() {
-		if (this.props.results.length > 0) {
+		console.log(this.results)
+
+		if (this.results.length > 0) {
 			return(
 				<div class="results-container">
 					<a class="counter" onClick={this.scrollToResults}>
-						<span>{this.props.results.length} Ergebnisse</span>
+						<span>{this.results.length} Ergebnisse</span>
 					</a>
+					<div class="sort"
+						onClick={this.onSortByNameClick}
+						//onSortByNameClick={this.sortResults}
+						>Sort by Name</div>
 					<ul class="results">
-						{this.props.results.map((data, i) => {
+						{this.results.map((data, i) => {
 							return (
 								<li class="result-item" key={i}>
 									{React.cloneElement(this.props.children, { data: data })}
+									{console.log(data)}
 							</li>
 							)
 						})}
@@ -37,4 +85,16 @@ export default class Results extends React.Component {
 		}
 	}
 }
+
+// class ResultsTable extends React.Component {
+// 		constructor(props) {
+// 		super(props)
+// 	}
+
+// 	render(){
+// 		return(
+
+// 		)
+// 	}
+// }
 
